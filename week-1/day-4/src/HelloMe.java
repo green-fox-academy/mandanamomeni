f018bba4ca7c95ef4c80f4186f2b126d366bd92e
repo <mandana.momenii@gmail.com>
2018-08-02 14:31:07 +0200

    first commit

---
+++ week-1/day-4/src/HelloMe.java
@@ -0,0 +1,14 @@
+public class HelloMe {
+  public static void main(String[] args) {
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    System.out.println("Hello Csabi!");
+    //Hello, Esther!
+    System.out.println("Hello Esther!");
+    // Hello, Mary!
+    System.out.println("Hello Mary!");
+    // Hello, Joe!
+    System.out.println("Hello Joe!");
+  }
+}
---
+++ week-1/day-4/src/IntroduceYourself.java
@@ -0,0 +1,30 @@
+public class IntroduceYourself {
+  public static void main(String[] args) {
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+
+    String myName = "Mandana Momeni";
+    int myAge = 33 ;
+    int myHeight  = 162 ;
+
+    System.out.println(myName);
+    System.out.println(myAge);
+    System.out.println(myHeight);
+
+
+   /* System.out.println("Mandana Momeni");
+    System.out.println(33);
+    System.out.println(162);
+    */
+
+
+  }
+}
