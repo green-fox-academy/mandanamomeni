cd9428cf31bc6eaffed92ad09fe163557e8811a5
repo <mandana.momenii@gmail.com>
2018-08-11 17:42:55 +0200

    addind Lineplay and envelopestar

--- week-02/day-05/src/Lineplay.java
+++ week-02/day-05/src/Lineplay.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Lineplay {
+
+  public static void mainDraw(Graphics graphics) {
+
+    int x = 0;
+
+    for (int i = 0; i < 13; i++) {
+
+      graphics.setColor(Color.GRAY);
+      graphics.drawLine(x + 20, 5, 280, 5 + x);
+      x += 20;
+    }
+    int y = 0;
+  for (int i = 0 ; i<13; i++){
+  y += 20;
+
+    graphics.setColor(Color.PINK);
+    graphics.drawLine(5, y+20, 5+y , 280);
+    x += 20;
+  }
+
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 400;
+  static int HEIGHT = 400;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
 }
--- week-02/day-05/src/envelopeStar.java
+++ week-02/day-05/src/envelopeStar.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class envelopeStar {
+
+  public static void mainDraw(Graphics graphics) {
+
+    int x1 = 0;
+    int y1 = 200;
+    int x2 = 200;
+    int y2 = 200;
+    for (int i = 0; i < 11; i++) {
+
+      graphics.setColor(Color.RED);
+      graphics.drawLine(x1, y1, x2, y2);
+      x1 += 20;
+      y2 -= 20;
+    }
+  }
+//    int x12 = 0;
+//    int y12 = 200;
+//    int x22 = 200;
+//    int y22 = 200;
+//    for(int i = 0; i<11; i++) {
+//
+//      graphics.setColor(Color.RED);
+//      graphics.drawLine (x12 , y12, x22, y22);
+//      x22 +=20  ;
+//      y12 -=20 ;
+//    }
+//  }
+
+  // Don't touch the code below
+  static int WIDTH = 400;
+  static int HEIGHT = 400;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
 }
