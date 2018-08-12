81416050ac2f81446af5279ad541e90cc8daea75
repo <mandana.momenii@gmail.com>
2018-08-12 18:23:57 +0200

    write a method

--- week-02/day-02/src/SolarSystem.java
+++ week-02/day-02/src/SolarSystem.java
@@ -5,22 +5,20 @@
 
 //System.out.println(putSaturn(planetList));
 // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+// Saturn is missing from the planetList
+// Insert it into the correct position
+// Create a method called putSaturn() which has list parameter and returns the correct list.
+// Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+
 import java.util.*;
 
 public class SolarSystem {
   public static void main(String... args) {
-
-
-
-
-
-
     ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
-      planetList.add("Saturn");
-    System.out.println(planetList);
-
-
-
-
+    System.out.println(putSaturn(planetList));
+  }
+  public static ArrayList<String> putSaturn(ArrayList<String> list) {
+    list.add("saturn");
+    return list;
   }
 }
