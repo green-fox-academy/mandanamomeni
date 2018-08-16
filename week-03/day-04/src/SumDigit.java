bf95bb8ff0a0e9a572fde8cb285f3033ffd2a3c6
repo <mandana.momenii@gmail.com>
2018-08-16 12:10:58 +0200

    better numberpower

--- week-03/day-04/src/SumDigit.java
+++ week-03/day-04/src/SumDigit.java
@@ -1,7 +1,3 @@
-// Given a non-negative int n, return the sum of its digits recursively (no loops).
-// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
-// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
-
 public class SumDigit {
   public static void main(String[] args) {
     // Given base and n that are both 1 or more, compute recursively (no loops)
